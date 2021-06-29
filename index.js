e2eac699980cb317fc41e78ddd276e2c1e744069
Mark Cabanes <cabanesdev@gmail.com>
2021-06-29 23:28:03 +0200

    Change variables' name

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-	let audio = document.getElementById("song");
+document.addEventListener('DOMContentLoaded', function () {
+	let audio = document.getElementById('song');
 	audio.loop = true;
 	audio.volume = 1;
-	let volume = document.getElementById("volume");
-	volume.addEventListener("click", function() {
-			if (audio.paused) {
-					audio.play()
-					this.children[0].classList.remove("fa-play");
-					this.children[0].classList.add("fa-pause");
-			}
-			else {
-					audio.pause();
-					this.children[0].classList.remove("fa-pause");
-					this.children[0].classList.add("fa-play");			
-				}
+	let play = document.getElementById('play');
+	play.addEventListener('click', function () {
+		if (audio.paused) {
+			audio.play();
+			this.children[0].classList.remove('fa-play');
+			this.children[0].classList.add('fa-pause');
+		} else {
+			audio.pause();
+			this.children[0].classList.remove('fa-pause');
+			this.children[0].classList.add('fa-play');
+		}
 	});
 });
